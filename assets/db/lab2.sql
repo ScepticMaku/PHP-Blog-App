3defa7b7646904bca3eb8d7fc85d1b9f8391abc7
ScepticMaku <markjayvincentcortes@gmail.com>
2025-04-13 20:11:00 +0800

    updated db

--- assets/db/lab2.sql
+++ assets/db/lab2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 13, 2025 at 12:39 PM
+-- Generation Time: Apr 13, 2025 at 02:10 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -31,17 +31,19 @@ CREATE TABLE `posts` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `content` text NOT NULL,
-  `author_id` int(11) NOT NULL
+  `author_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `posts`
 --
 
-INSERT INTO `posts` (`id`, `title`, `content`, `author_id`) VALUES
-(3, 'ughh', 'ughhhhhhhhhhhhhhhhh', 10),
-(4, 'Sarap', 'sarap mo markj\r\n', 11),
-(5, 'BAYOT KO', 'AKO AY ISANG BADING', 12);
+INSERT INTO `posts` (`id`, `title`, `content`, `author_id`, `created_at`, `updated_at`) VALUES
+(3, 'ughh', 'ughhhhhhhhhhhhhhhhh', 10, '2025-04-13 12:08:13', NULL),
+(4, 'Sarap', 'sarap mo markj\r\n', 11, '2025-04-13 12:08:13', NULL),
+(5, 'BAYOT KO', 'AKO AY ISANG BADING', 12, '2025-04-13 12:08:13', NULL);
 
 -- --------------------------------------------------------
 
@@ -51,22 +53,24 @@ INSERT INTO `posts` (`id`, `title`, `content`, `author_id`) VALUES
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `password`) VALUES
-(4, 'Markj', 'Cortes', 'markj', '$2y$10$wagaeHx2fx4sKV5kUJSs5OTxzWFd7FdW.uL7tbwyvNs9VZkEi5O9i'),
-(5, 'test', 'test', 'test', '$2y$10$OQHSXyV6SHcuvQPSZqvgc.2FVC5le4ZrnnYVYBRcOuFAKOsedwx5e'),
-(10, 'Rashid', 'Tapales', 'Snezhy', '$2y$10$fccSwKvDyLFqtwqO/WmKD.3NXSDrApp3GH.vMIK.LIvryKyKbPe7e'),
-(11, 'Axci', 'Cabusas', 'sumkai', '$2y$10$RuTxNPPePuK.3AFyti7DQuEqIr78QiiCkVqDRR7oyPr8H4hfzwkSO'),
-(12, 'Aljon', 'Paragoso', 'aljon', '$2y$10$8etoXwUcenOJ89Zm.go96O8na4leMBHrtQXT58pWmPPCMfjZ4L5Ky');
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `username`, `password`, `created_at`, `updated_at`) VALUES
+(4, 'Markj', 'Cortes', 'markj', '$2y$10$wagaeHx2fx4sKV5kUJSs5OTxzWFd7FdW.uL7tbwyvNs9VZkEi5O9i', '2025-04-13 12:06:58', NULL),
+(5, 'test', 'test', 'test', '$2y$10$OQHSXyV6SHcuvQPSZqvgc.2FVC5le4ZrnnYVYBRcOuFAKOsedwx5e', '2025-04-13 12:06:58', NULL),
+(10, 'Rashid', 'Tapales', 'Snezhy', '$2y$10$fccSwKvDyLFqtwqO/WmKD.3NXSDrApp3GH.vMIK.LIvryKyKbPe7e', '2025-04-13 12:06:58', NULL),
+(11, 'Axci', 'Cabusas', 'sumkai', '$2y$10$RuTxNPPePuK.3AFyti7DQuEqIr78QiiCkVqDRR7oyPr8H4hfzwkSO', '2025-04-13 12:06:58', NULL),
+(12, 'Aljon', 'Paragoso', 'aljon', '$2y$10$8etoXwUcenOJ89Zm.go96O8na4leMBHrtQXT58pWmPPCMfjZ4L5Ky', '2025-04-13 12:06:58', NULL);
 
 --
 -- Indexes for dumped tables
